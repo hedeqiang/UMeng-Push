5d740626318f1612dd1c93f3637e4334eb1700f1
hedeqiang <laravel_code@163.com>
2019-04-03 11:02:55 +0000

    Apply fixes from StyleCI

--- src/Exceptions/InvalidArgumentException.php
+++ src/Exceptions/InvalidArgumentException.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the hedeqiang/umeng.
+ *
+ * (c) hedeqiang <laravel_code@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Hedeqiang\UMeng\Exceptions;
 
