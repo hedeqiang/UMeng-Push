bf80310933619441ef99f805071c77ebde17a4d5
hedeqiang <laravel_code@163.com>
2019-04-23 22:19:39 +0800

    test

---
+++ tests/NotificationTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hedeqiang\UMeng\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class NotificationTest extends TestCase
+{
+    public function testsendAndroidCustomizedcast()
+    {
+
+    }
+
+    public function testsendAndroidBroadcast()
+    {
+
+    }
+}
