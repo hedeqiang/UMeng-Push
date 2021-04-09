0b5aaa41bb0873ac4db64933be886fa35e380a76
hedeqiang <laravel_code@163.com>
2021-04-09 18:02:46 +0800

    init v2.x

---
+++ src/Push.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Hedeqiang\UMeng;
+
+use Hedeqiang\UMeng\Exceptions\HttpException;
+use Hedeqiang\UMeng\Traits\HasHttpRequest;
+
+class Push
+{
+    use HasHttpRequest;
+
+    //  消息发送
+    const ENDPOINT_TEMPLATE_SEND = 'https://msgapi.umeng.com/api/send';
+    // 任务类消息状态查询
+    const ENDPOINT_TEMPLATE_STATUS = 'https://msgapi.umeng.com/api/status';
+    // 任务类消息取消
+    const ENDPOINT_TEMPLATE_CANCEL = 'https://msgapi.umeng.com/api/cancel';
+    // 文件上传
+    const ENDPOINT_TEMPLATE_UPLOAD = 'https://msgapi.umeng.com/upload';
+
+    protected $config;
+
+    public function __construct(array $config)
+    {
+        $this->config = new Config($config);
+    }
+
+
+    /**
+     * 消息发送
+     * @param array $params
+     * @return mixed
+     */
+    public function send($params = []): array
+    {
+        list($url, $params) = $this->getUrl($params, 'send');
+        return $this->curl($url, $params);
+    }
+
+    /**
+     * 任务类消息状态查询
+     * @param array $params
+     * @return mixed
+     */
+    public function status(array $params): array
+    {
+        list($url, $params) = $this->getUrl($params, 'status');
+        return $this->curl($url, $params);
+    }
+
+    /**
+     * 任务类消息取消
+     * @param array $params
+     * @return mixed
+     */
+    public function cancel(array $params): array
+    {
+        list($url, $params) = $this->getUrl($params, 'cancel');
+        return $this->curl($url, $params);
+    }
+
+    /**
+     * 文件上传
+     * @param array $params
+     * @return mixed
+     */
+    public function upload(array $params): array
+    {
+        list($url, $params) = $this->getUrl($params, 'upload');
+        return $this->curl($url, $params);
+    }
+
+
+    /**
+     * Build endpoint url.
+     *
+     * @param array $body
+     * @param string $type
+     * @return string
+     */
+    protected function buildEndpoint(array $body, string $type): string
+    {
+        $body = json_encode($body, true);
+        switch ($type) {
+            case 'send':
+                return $this->getSign(self::ENDPOINT_TEMPLATE_SEND, $body);
+            case 'status':
+                return $this->getSign(self::ENDPOINT_TEMPLATE_STATUS, $body);
+            case 'cancel':
+                return $this->getSign(self::ENDPOINT_TEMPLATE_CANCEL, $body);
+            case 'upload':
+                return $this->getSign(self::ENDPOINT_TEMPLATE_UPLOAD, $body);
+        }
+    }
+
+    /**
+     * @param string $endpoint
+     * @param $body
+     * @return string
+     */
+    protected function getSign(string $endpoint, $body): string
+    {
+        switch ($this->config->get('deviceType'))
+        {
+            case 'Android':
+                $sign = md5('POST' . $endpoint . $body . $this->config->get('Android.appMasterSecret'));
+                return $endpoint . '?sign=' . $sign;
+            case 'iOS':
+                $sign = md5('POST' . $endpoint . $body . $this->config->get('iOS.appMasterSecret'));
+                return $endpoint . '?sign=' . $sign;
+//            case 'All':
+//                $android_sign = md5('POST' . $endpoint . $body . $this->config->get('Android.appMasterSecret'));
+//                $ios_sign = md5('POST' . $endpoint . $body . $this->config->get('iOS.appMasterSecret'));
+//                return $endpoint . '?sign=' . $android_sign;
+        }
+
+    }
+
+    /**
+     * 获取 URL 和参数
+     * @param array $params
+     * @param string $type
+     * @return array
+     */
+    protected function getUrl(array $params, string $type): array
+    {
+        if (!array_key_exists('appKey', $params)) {
+            switch ($this->config->get('deviceType'))
+            {
+                case 'Android':
+                    $params['appkey'] = $this->config->get('Android.appKey');
+                    break;
+                case 'iOS':
+                    $params['appkey'] = $this->config->get('iOS.appKey');
+                    break;
+            }
+        }
+        
+        if (!array_key_exists('timestamp', $params)) {
+            $params['timestamp'] = time();
+        }
+        return [$this->buildEndpoint($params, $type), $params];
+    }
+
+
+    /**
+     * @param $url
+     * @param $params
+     * @return array|mixed
+     */
+    protected function curl(string $url, $params): array
+    {
+        try {
+            $response = $this->postJson($url, $params);
+        } catch (\Exception $e) {
+            $response             = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+            return json_decode($responseBodyAsString, true);
+        }
+        return json_decode((string)$response, true);
+    }
+}
---
+++ src/Traits/HasHttpRequest.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Hedeqiang\UMeng\Traits;
+
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Trait HasHttpRequest.
+ */
+trait HasHttpRequest
+{
+    /**
+     * Make a get request.
+     *
+     * @param array $query
+     * @param array $headers
+     *
+     * @return array
+     */
+    protected function get(string $endpoint, $query = [], $headers = [])
+    {
+        return $this->request('get', $endpoint, [
+            'headers' => $headers,
+            'query' => $query,
+        ]);
+    }
+
+    /**
+     * Make a post request.
+     *
+     * @param $options
+     * @param array $headers
+     *
+     * @return array
+     */
+    protected function post(string $endpoint, $options, $headers = [])
+    {
+        return $this->request('post', $endpoint, [
+            'headers' => $headers,
+            'multipart' => $options,
+        ]);
+    }
+
+    /**
+     * Make a post request with json params.
+     *
+     * @param       $endpoint
+     * @param array $params
+     * @param array $headers
+     *
+     * @return array
+     */
+    protected function postJson($endpoint, $params = [], $headers = [])
+    {
+        return $this->request('post', $endpoint, [
+            'headers' => $headers,
+            'json' => $params,
+        ]);
+    }
+
+    /**
+     * @param $endpoint
+     * @param array $headers
+     * @param array $query
+     *
+     * @return array
+     */
+    protected function delete($endpoint, $headers = [], $query = [])
+    {
+        return $this->request('delete', $endpoint, [
+            'headers' => $headers,
+            'query' => $query,
+        ]);
+    }
+
+    /**
+     * @param $endpoint
+     * @param $params
+     * @param array $headers
+     *
+     * @return array
+     */
+    protected function put($endpoint, $params, $headers = [])
+    {
+        return $this->request('put', $endpoint, [
+            'headers' => $headers,
+            'json' => $params,
+        ]);
+    }
+
+    /**
+     * Make a http request.
+     *
+     * @param array $options http://docs.guzzlephp.org/en/latest/request-options.html
+     *
+     * @return array
+     */
+    protected function request(string $method, string $endpoint, $options = [])
+    {
+        return $this->unwrapResponse($this->getHttpClient($this->getBaseOptions())->{$method}($endpoint, $options));
+    }
+
+    /**
+     * Return base Guzzle options.
+     *
+     * @return array
+     */
+    protected function getBaseOptions()
+    {
+        return [
+            'base_uri' => method_exists($this, 'getBaseUri') ? $this->getBaseUri() : '',
+            'timeout' => method_exists($this, 'getTimeout') ? $this->getTimeout() : 10.0,
+        ];
+    }
+
+    /**
+     * Return http client.
+     *
+     * @return \GuzzleHttp\Client
+     *
+     * @codeCoverageIgnore
+     */
+    protected function getHttpClient(array $options = [])
+    {
+        return new Client($options);
+    }
+
+    /**
+     * Convert response contents to json.
+     *
+     * @return ResponseInterface|array|string
+     */
+    protected function unwrapResponse(ResponseInterface $response)
+    {
+        $contentType = $response->getHeaderLine('Content-Type');
+        $contents = $response->getBody()->getContents();
+        if (false !== stripos($contentType, 'json') || stripos($contentType, 'javascript')) {
+            return json_decode($contents, true);
+        } elseif (false !== stripos($contentType, 'xml')) {
+            return json_decode(json_encode(simplexml_load_string($contents)), true);
+        }
+
+        return $contents;
+    }
+}
