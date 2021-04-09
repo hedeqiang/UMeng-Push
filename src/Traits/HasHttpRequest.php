20b8f2d4bb5d2f03b9046e36ea1f8c30574e0921
hedeqiang <hedeqiang@users.noreply.github.com>
2021-04-09 10:03:26 +0000

    Apply fixes from StyleCI

--- src/Traits/HasHttpRequest.php
+++ src/Traits/HasHttpRequest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the hedeqiang/umeng.
+ *
+ * (c) hedeqiang <laravel_code@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Hedeqiang\UMeng\Traits;
 
 use GuzzleHttp\Client;
