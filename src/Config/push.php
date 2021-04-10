97fff5f472481b85488e84d98cc9a229dd089aa4
hedeqiang <laravel_code@163.com>
2021-04-10 11:26:13 +0800

    push

--- src/Config/push.php
+++ src/Config/push.php
@@ -11,13 +11,13 @@
 
 return [
     'Android' => [
-        'appKey' =>  env('ANDROID_PUSH_APP_KEY'),
-        'appMasterSecret' => env('ANDROID_PUSH_APP_MASTER_SECRET'),
-        'production_mode' => env('ANDROID_PUSH_PRODUCTION_MODE'),
+        'appKey' =>  env('ANDROID_PUSH_APP_KEY',''),
+        'appMasterSecret' => env('ANDROID_PUSH_APP_MASTER_SECRET',''),
+        'production_mode' => env('ANDROID_PUSH_PRODUCTION_MODE',true),
     ],
     'iOS' => [
-        'appKey' =>  env('IOS_PUSH_APP_KEY'),
-        'appMasterSecret' => env('IOS_PUSH_APP_MASTER_SECRET'),
-        'production_mode' => env('IOS_PUSH_PRODUCTION_MODE'),
+        'appKey' =>  env('IOS_PUSH_APP_KEY',''),
+        'appMasterSecret' => env('IOS_PUSH_APP_MASTER_SECRET',''),
+        'production_mode' => env('IOS_PUSH_PRODUCTION_MODE',true),
     ]
 ];
