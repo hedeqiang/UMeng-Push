360d753e7a470a9bd6a42d4ac8d949e00e164bbc
hedeqiang <31909061+hedeqiang@users.noreply.github.com>
2021-04-10 11:34:22 +0800

    Update and rename NotificationTest.php to PushTest.php

---
+++ tests/PushTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the hedeqiang/umeng.
+ *
+ * (c) hedeqiang <laravel_code@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Hedeqiang\UMeng\Tests;
+
+use Hedeqiang\UMeng\Android;
+use PHPUnit\Framework\TestCase;
+
+class PushTest extends TestCase
+{
+   
+}
